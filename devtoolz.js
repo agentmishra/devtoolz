74093dedc00a13d8c95704321d211761ada4ef72
Jonathan Griffin <griffiti93@gmail.com>
2016-10-08 05:48:53 +0100

    Started working on subcommands structure.

--- devtoolz.js
+++ devtoolz.js
@@ -1,1 +1,22 @@
 #!/usr/local/bin/node --harmony
+
+var _         = require('lodash');
+var fs        = require('fs');
+var program   = require('commander');
+var pkgInfo   = require('pkginfo')(module);
+var Options   = require('options');
+
+program
+    .version(module.exports.version)
+    .option('-c, --config <file>', 'path to configuration file - default path is current directory');
+
+program
+    .command('refresh <project|workspace>')
+    .description('Perform a merge status on target branch for configured projects.')
+    .option('-b, --build', 'perform build for target project or workspace')
+    .action(function(target, options) {
+        console.log('target: ' + target);
+        process.exit(0);
+    });
+
+program.parse(process.argv);
